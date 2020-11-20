4f163da38ca8f6f455b11fdbe3ccedfd73f149a0
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-20 10:28:11 -0800

    commit tree 123a7c4a57f43f6428486707a3332ec64f418bda

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,30 @@
 
 
+def my_collect(arrays)
+    collection = []
+
+    i = 0
+    while i < arrays.length
+        collection << yield(arrays[i])
+        i += 1
+    end
+    collection
+end 
+
+ 
+
+
+
+# my_collect
+#   can handle an empty collection
+#   yields the correct element from a given collection, in this case languages
+#   returns a new collection of appropriately modified elements, in this case capitalized languages
+#   does not modify the original collection
+#   yields the correct element from the given collection, in this case students
+#   returns a new collection of appropriately modified elements, in this case student first names
+#   does not modify the original collection
+
+
+
+
+
